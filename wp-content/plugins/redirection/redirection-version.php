40df0fc4eee6798b99abb0102bbecddfac00b8e1
Hans Fredrik Nordhaug <hansfn@gmail.com>
2020-06-03 14:25:16 +0200

    Updated WP core to 5.4.1.
    Updated several plugins and themes too.

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.5.1' );
-define( 'REDIRECTION_BUILD', '77207e69fee0b331324f0be84e962d5c' );
+define( 'REDIRECTION_VERSION', '4.8' );
+define( 'REDIRECTION_BUILD', '7acce5951ed37d0a438ffb92e849136e' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
